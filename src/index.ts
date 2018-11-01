e27ce84af76df7b84cde7cc32c73197bda0ee953
Oliver Widder <oliver.widder@gmail.com>
2018-11-01 19:37:01 +0100

    remove files

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import {createTable} from './table/simpleTable';
+
+export const superSimpleUtils = {
+    createTable,
+} as any;
+
+(window as any).superSimpleUtils = superSimpleUtils;
---
+++ src/table/simpleTable.ts
@@ -0,0 +1,50 @@
+import * as d3 from 'd3';
+import * as _ from 'lodash';
+
+import 'materialize-css/dist/css/materialize.css';
+
+interface StringMap {
+    [key: string]: string | number;
+}
+
+const getHeadlines = (data: StringMap[]) => {
+    const headlines: string[] = [];
+    data.forEach(element => {
+        _.keys(element).forEach(key => {
+            if(headlines.indexOf(key) < 0) {
+                headlines.push(key);
+            }
+        })
+    })
+
+    return headlines;
+}
+
+export const createTable = (selector: string, data: StringMap[]) => {
+    const headlines = getHeadlines(data);
+
+    const root = d3.select(selector);
+
+    const trhead = root
+        .append("table")
+        .attr("class", "responsive-table")
+        .append("thead")
+        .append("tr");
+
+    trhead.selectAll("th")
+        .data(headlines)
+        .enter()
+        .append("th")
+        .text(d => d);
+
+    const tbody = root.select("table").append("tbody");
+
+    const trbody = tbody.selectAll("tr")
+        .data(data)
+        .enter()
+        .append("tr");
+
+    headlines.forEach(headline => {
+        trbody.append("td").text(d => _.isUndefined(d[headline]) ? "" : d[headline]);
+    })
+}
