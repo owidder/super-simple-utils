3c9a138a9a584aef636d8682e1693234ddd63b86
Oliver Widder <oliver.widder@gmail.com>
2019-09-01 20:08:19 +0200

    fix table

--- src/showDataAsTable/showDataAsTable.ts
+++ src/showDataAsTable/showDataAsTable.ts
@@ -17,37 +17,60 @@ const getHeadlines = (data: StringMap[]): string[] => {
     return headlines;
 }
 
-export const showDataAsTable = (selector: string, data: StringMap[], idAttributeName?: string, tableClass?: string) => {
-    const headlines = getHeadlines(data);
-    const root = d3.select(selector);
+type Selection = d3.Selection<d3.BaseType, unknown, HTMLElement, any>;
 
-    const trhead = root
+const createTableWithHeadRowAndBody = (root: Selection, tableClass: string): void => {
+    const table = root.selectAll("table")
+        .data([1])
+        .enter()
         .append("table")
-        .attr("class", tableClass)
-        .append("thead")
+        .attr("class", tableClass);
+    table.append("thead")
         .append("tr");
+    table.append("tbody");
+}
 
+const createHeadRowData = (root: Selection, headlines: string[]): void => {
     // @ts-ignore (WebStorm thinks this is not correct, but it is)
-    const headDataSelection = trhead.selectAll("th").data(headlines, d => d);
+    const headDataSelection = root.select("table thead tr").selectAll("th").data(headlines, d => d)
 
     headDataSelection
         .enter()
         .append("th")
-        .text(d => d);
+        .text(d => d)
 
-    headDataSelection.exit().remove();
+    headDataSelection.exit().remove()
+}
 
-    const tbody = root.select("table").append("tbody");
+const createTableBodyRows = (root: Selection, data: StringMap[], idAttributeName?: string): void => {
+    const bodyDataSelection = root.select("table tbody").selectAll("tr")
+        .data(data, idAttributeName ? d => d[idAttributeName] : undefined)
 
-    const bodyDataSelection = tbody.selectAll("tr").data(data, idAttributeName ? d => d[idAttributeName] : undefined);
+    const trbody = bodyDataSelection.enter().append("tr")
 
-    const trbody = bodyDataSelection.enter().append("tr");
+    bodyDataSelection.exit().remove()
+}
 
-    headlines.forEach(headline => {
-        trbody.append("td").text(d => d[headline] ? "" : d[headline]);
-    });
+const createTableBodyData = (root: Selection, headlines: string[]): void => {
+    const tableBodyDataSelection = root.selectAll("table tbody tr").selectAll("td")
+        .data(d => headlines.map(headline => d[headline]), (_d, i) => headlines[i])
 
-    bodyDataSelection.exit().remove();
+    tableBodyDataSelection.enter()
+        .append("td")
+        .merge(tableBodyDataSelection)
+        .text(d => d)
+
+    tableBodyDataSelection.exit().remove()
+}
+
+export const showDataAsTable = (selector: string, data: StringMap[], idAttributeName?: string, tableClass?: string) => {
+    const headlines = getHeadlines(data);
+    const root = d3.select(selector);
+
+    createTableWithHeadRowAndBody(root, tableClass);
+    createHeadRowData(root, headlines);
+    createTableBodyRows(root, data, idAttributeName);
+    createTableBodyData(root, headlines);
 }
 
 (window as any).showDataAsTable = showDataAsTable;
