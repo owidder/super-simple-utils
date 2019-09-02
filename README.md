e0fa6ed510fe083d064e2153d43cebb637c8e9a7
Oliver Widder <oliver.widder@gmail.com>
2019-09-02 08:06:08 +0200

    readme

--- README.md
+++ README.md
@@ -1,19 +1,40 @@
 ## just some utils (super simple)
-* showDataAsTable
+
+### showDataAsTable
+* `showDataAsTable(cssSelector: string, 
+    dataArray: {[key: string]: string | number}, 
+    nameOfIdAttribute?: string, 
+    cssClassesForTable?: string)`
+
 ```
 <body>
 <div id="root"></div>
 
 <script>
     window.onload = function () {
-        const data = [
-            {a:1, b:2, c:3},
-            {a:5, d:4, e:8},
-            {b:1, c:4, e:9}
+        const data1 = [
+            {nr:1, a:1, b:2, c:3},
+            {nr:2, a:5, d:4, e:8},
+            {nr:3, b:1, c:4, e:9},
         ]
 
-        window.showDataAsTable("#root", data);
+        window.showDataAsTable("#root", data1, "nr");
+
+        setTimeout(() => {
+            const data2 = [
+                {nr:1, a:1, b:2, c:3},
+                {nr:3, b:1, c:4, f:10},
+                {nr:4, b:1, f:11, c:5},
+            ]
+
+            window.showDataAsTable("#root", data2, "nr");
+        }, 2000)
     }
 </script>
 </body>
 ```
+
+### hashSHA256
+* `hashSHA256(text: string): string`
+
+`const abcHashed = hashSHA256("abc")`
--- src/table/showDataAsTable.ts
+++ src/table/showDataAsTable.ts
@@ -4,7 +4,7 @@ interface StringMap {
     [key: string]: string | number;
 }
 
-const getHeadlines = (data: StringMap[]) => {
+const getHeadlines = (data: StringMap[]): string[] => {
     const headlines: string[] = [];
     data.forEach(element => {
         Object.keys(element).forEach(key => {
@@ -17,33 +17,60 @@ const getHeadlines = (data: StringMap[]) => {
     return headlines;
 }
 
-export const showDataAsTable = (selector: string, data: StringMap[], idAttributeName?: string) => {
+type Selection = d3.Selection<d3.BaseType, unknown, HTMLElement, any>;
+
+const createTableWithHeadRowAndBody = (root: Selection, tableClass: string): void => {
+    const table = root.selectAll("table")
+        .data([1])
+        .enter()
+        .append("table")
+        .attr("class", tableClass);
+    table.append("thead")
+        .append("tr");
+    table.append("tbody");
+}
+
+const createHeadRowData = (root: Selection, headlines: string[]): void => {
+    // @ts-ignore (WebStorm thinks this is not correct, but it is)
+    const headDataSelection = root.select("table thead tr").selectAll("th").data(headlines, d => d)
+
+    headDataSelection
+        .enter()
+        .append("th")
+        .text(d => d)
+
+    headDataSelection.exit().remove()
+}
+
+const createTableBodyRows = (root: Selection, data: StringMap[], idAttributeName?: string): void => {
+    const bodyDataSelection = root.select("table tbody").selectAll("tr")
+        .data(data, idAttributeName ? d => d[idAttributeName] : undefined)
+
+    const trbody = bodyDataSelection.enter().append("tr")
+
+    bodyDataSelection.exit().remove()
+}
+
+const createTableBodyData = (root: Selection, headlines: string[]): void => {
+    const tableBodyDataSelection = root.selectAll("table tbody tr").selectAll("td")
+        .data(d => headlines.map(headline => d[headline]), (_d, i) => headlines[i])
+
+    tableBodyDataSelection.enter()
+        .append("td")
+        .merge(tableBodyDataSelection)
+        .text(d => d)
+
+    tableBodyDataSelection.exit().remove()
+}
+
+export const showDataAsTable = (selector: string, data: StringMap[], idAttributeName?: string, tableClass?: string) => {
     const headlines = getHeadlines(data);
     const root = d3.select(selector);
 
-    const trhead = root
-        .append("table")
-        .attr("class", "responsive-table striped")
-        .append("thead")
-        .append("tr");
-
-    trhead.selectAll("th")
-        .data(headlines)
-        .enter()
-        .append("th")
-        .text(d => d);
-
-    const tbody = root.select("table").append("tbody");
-
-    const dataSelection = tbody.selectAll("tr").data(data, idAttributeName ? d => d[idAttributeName] : undefined);
-
-    const trbody = dataSelection.enter().append("tr");
-
-    headlines.forEach(headline => {
-        trbody.append("td").text(d => d[headline] ? "" : d[headline]);
-    });
-
-    dataSelection.exit().remove();
+    createTableWithHeadRowAndBody(root, tableClass);
+    createHeadRowData(root, headlines);
+    createTableBodyRows(root, data, idAttributeName);
+    createTableBodyData(root, headlines);
 }
 
 (window as any).showDataAsTable = showDataAsTable;
